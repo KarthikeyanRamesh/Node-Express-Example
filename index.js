c49e0edeb5c596377b744e83e97944ab9ed4ccc3
KarthikeyanRamesh <karthisujatha10@gmail.com>
2022-10-02 17:06:41 +0530

    added leader, promo Routers

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
-const express = require('express'),
-     http = require('http');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-
-const dishRouter = require('./routes/dishRouter');
-
+const express = require('express');
+const http = require('http');
 const hostname = 'localhost';
 const port = 3000;
+const morgan = require('morgan');
+const bodyParser =  require('body-parser');
+
+const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 
 const app = express();
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 
-app.use(express.static(__dirname+'/public'))
+app.use(express.static(__dirname+ '/public'));
 
-app.use((req, res, next)=> {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<html><body><h1>Express server is running in port http://localhost:3000/ </h1></body></html>');
-})
+app.use( (req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>This is an express server</h1></body></html>');
+});
+
 const server = http.createServer(app);
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, hostname, () =>{
+    console.log(`Server running at http://${hostname}:${port}`);
 });
